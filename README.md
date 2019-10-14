55c0296e55737c991deb737e2b1cdd56ee612d65
kkpoon <kkpoon@eml.cc>
2019-10-14 22:30:57 +0800

    Bump android version

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # CCCSTC App
 
 This is an app in [flutter](https://flutter.io).
+
+## Build Android APK
+
+```
+flutter build apk
+```
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/kkpoon/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/kkpoon/Development/github.com/cccstc/app"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/kkpoon/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=3.0.2"
+export "FLUTTER_BUILD_NUMBER=3"
